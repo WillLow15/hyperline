e57b53d6fab2eccf39fce143aef9830c64e0ab1c
WillLow15 <45129709+WillLow15@users.noreply.github.com>
2019-07-04 02:37:43 +0200

    Update index.js

--- src/lib/plugins/index.js
+++ src/lib/plugins/index.js
@@ -1,11 +1,11 @@
 import hostname from './hostname'
 import ip from './ip'
 import memory from './memory'
-// Import Uptime from './uptime'
+import Uptime from './uptime'
 import cpu from './cpu'
 import network from './network'
 import battery from './battery'
-// Import Time from './time'
+import Time from './time'
 // Import Docker from './docker'
 import spotify from './spotify'
 
